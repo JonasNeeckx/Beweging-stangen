7aa5fff6c4755eb4fcd606e3db7ca0f623c97b53
NicolasHeintz <33027566+NicolasHeintz@users.noreply.github.com>
2018-05-10 12:49:16 +0200

    function for optimal excentricity

---
+++ optimalExcentricity.m
@@ -0,0 +1,33 @@
+function e_optimal = optimalExcentricity(S, V, R0)
+%OPTIMALEXCENTRICITY calculates the optimal excentricity for the cam by
+%examining the maximal pressure angle at every excentricity. As a check the
+%optimal excentricity should also be the excentricity where the maximal and
+%minimal pressure angle have the same amplitude. 
+%
+%INPUT
+%@param S
+%           S describes the elevation on the cam
+%@param V
+%           V describes the change in elevation on the cam
+%@param R0
+%           R0 is the pitch circle radius
+%
+%OUTPUT
+%@result e_optimal
+%           e_optimal is the optimal excentricity value
+
+optimal_alpha = inf;
+alpha = zeros(size(S));
+
+for e = -R0:0.01:R0 
+    disp(e);
+    for i = 1:size(S, 2)
+        alpha(i) = atan((V(i)-e)/(sqrt(R0^2-e^2)+S(i)));
+    end
+    max_alpha = max(abs(alpha));
+    if max_alpha < optimal_alpha
+        e_optimal = e;
+        optimal_alpha = max_alpha;
+        
+    end
+end
