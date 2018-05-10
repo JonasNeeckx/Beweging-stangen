f839cb4d1b56816bc4ad6075d0559de1feb84eae
NicolasHeintz <33027566+NicolasHeintz@users.noreply.github.com>
2018-05-10 12:56:23 +0200

    Added excentricity

--- Berekeningen.m
+++ Berekeningen.m
@@ -1,8 +1,18 @@
+%% Load parameters
 out = load('halfcycloiden.mat');
 F_inert = out.normalforce_acc;
 F_load = out.normalforce_load;
 Pressure_Angle = out.pressure_angle;
 rpm = out.rpm;
 Mass = out.mass;
+S = out.S;
+V = out.V;
+bcr = out.bcr;
+rof = out.rof;
+R0 = bcr + rof;
 
+%% Geometry of the follower
+%base circle and radius of the follower
 
+%excentricity
+e_optimal = optimalExcentricity(S,V,R0);
