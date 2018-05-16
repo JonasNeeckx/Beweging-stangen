cb12dc27642a22b5d0d4a5990c5c6e981f0f3ded
NicolasHeintz <33027566+NicolasHeintz@users.noreply.github.com>
2018-05-16 21:13:02 +0200

    Corrected spring

--- Berekeningen.m
+++ Berekeningen.m
@@ -17,6 +17,7 @@ normalForce = out.normalforce_tot;
 xcam = out.xcam;
 ycam = out.ycam;
 k = 331.8*cosd(25.33)/14.99;
+
 out_no_e = load('halfcycloiden_no_exc');
 Pressure_Angle_no_e = out_no_e.pressure_angle;
 V_no_e = out_no_e.V;
@@ -30,7 +31,7 @@ e_optimal = optimalExcentricity(S,V,R0);
 
 %% Rigid-body forces
 %dimensionalisation of the spring
-[Springconstant_optimal, Fv0_optimal] = spring(Lift, F_load, F_inert, Pressure_Angle);
+[Springconstant_optimal, Fv0_optimal] = spring(S, F_load, F_inert, Pressure_Angle);
 
 %instantaneous power
 instantanious_power = instantaniousPower(normalForce,Pressure_Angle,rpm,V);
--- spring.m
+++ spring.m
@@ -1,14 +1,16 @@
 function [Springconstant_optimal, Fv0_optimal] = spring(Lift, F_load, F_inert, Pressure_Angle)
 optimal_N = inf;
 
-for Fv0 = 50:0.01:300
-    Springconstant= (331.7 +Fv0 + 0.008841)/14.99;
-    N = (F_load + Fv0 + Springconstant*Lift + F_inert)/cos(Pressure_Angle);
-    Nmax=max(N);
-    Nmin=min(N);
-    if (Nmax < optimal_N) && (Nmin >= 0)
-        optimal_N = N;
-        Springconstant_optimal = Springconstant;
-        Fv0_optimal = Fv0;
+for Fv0 = 200:.1:300
+    for Springconstant = 1.5:0.01:3
+        N = F_load +  + F_inert + (Fv0*ones(size(F_load)) + Springconstant*Lift)./cos(Pressure_Angle);
+        Nmax=max(N);
+        Nmin=min(N);
+        if (Nmax < optimal_N) && (Nmin >= 0)
+            optimal_N = Nmax;
+            Springconstant_optimal = Springconstant;
+            Fv0_optimal = Fv0;
+        end
     end
 end
+disp(optimal_N);
